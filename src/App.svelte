4b4a25ac02830efeb324a03bfe7e7a995deefe6d
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-25 18:09:06 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,143 @@
 <script>
   import { onMount } from "svelte";
-  var data = []
-  const h = new Set()
-  var ht = []
+  var [data, tanarok, targyak, ht, orl, orak] = [[], [], [], [], [], []];
+  var [tanarset, targyset, orarendset] = [{}, {}, {}];
+  var selectedoszt;
+  var [mino, maxo] = [20, 0];
+  const napok = "H K Sz Cs P".split(" ");
+  const napnevek = "Hétfő Kedd Szerda Csütörtök Péntek".split(" ");
+  const h = new Set();
   onMount(async () => {
-    data = (await fetch(
-      "https://tomuwhu.github.io/orarend_generator_svelte/orarend.json"
-    ).then((v) => v.json())).orarend
-    data.forEach(v => {
-      h.add(v.osztaly)
-    })
+    data = (
+      await fetch(
+        "https://tomuwhu.github.io/orarend_generator_svelte/orarend.json",
+      ).then((v) => v.json())
+    ).orarend;
+    data.forEach((v) => {
+      h.add(v.osztaly);
+    });
     ht = Array.from(h)
-  })
+      .filter((v) => !v.includes(","))
+      .sort();
+    tanarok = (
+      await fetch(
+        "https://tomuwhu.github.io/orarend_generator_svelte/tanarok.json",
+      ).then((v) => v.json())
+    ).tanarok;
+    tanarok.forEach(
+      (v) =>
+        (tanarset[v.ok] = v.tit?.trim()
+          ? `${v.tit} ${v.familyname} ${v.firstname}`
+          : `${v.familyname} ${v.firstname}`),
+    );
+    targyak = (
+      await fetch(
+        "https://tomuwhu.github.io/orarend_generator_svelte/targyak.json",
+      ).then((v) => v.json())
+    ).targyak;
+    targyak.forEach((v) => {
+      let [c, ...kl] = v.ttrov.toLowerCase();
+      if (!";.".includes(c)) kl = c + kl.join("");
+      else kl = kl.join("");
+      targyset[kl] = v.ttnev;
+    });
+  });
+  const terem = (ora, nap) =>
+    orl.filter((v) => v.ora === ora).filter((v) => v.nap === nap)[0]?.terem ||
+    "";
+  const tanar = (ora, nap) => {
+    let tk =
+      orl.filter((v) => v.ora === ora).filter((v) => v.nap === nap)[0]?.tanar ||
+      "-";
+    if (tk === "-") return "";
+    let tanarnev = tanarset[tk];
+    if (tanarnev?.length) return tanarnev;
+    return tk;
+  };
+  const targy = (ora, nap) => {
+    let tk =
+      orl.filter((v) => v.ora === ora).filter((v) => v.nap === nap)[0]?.targy ||
+      "-";
+    if (tk === "-") return "";
+    let targynev = targyset[tk.replaceAll("é", "e").replaceAll("á", "a").replaceAll("ö", "o")];
+    if (targynev?.length) return targynev;
+    return tk;
+  };
 </script>
 
 <main>
-  Cica
-  {#each ht as v}
-    Cica
-    {v}
-  {/each}
+  <select
+    bind:value={selectedoszt}
+    on:change={() => {
+      [mino, maxo] = [20, 0];
+      orak = [];
+      orl = data.filter(
+        (v) => v.osztaly.includes(selectedoszt) && v.het === "A",
+      );
+      orl.forEach((v) => {
+        if (mino > v.ora) mino = v.ora;
+        if (maxo < v.ora) maxo = v.ora;
+      });
+      for (let i = mino; i <= maxo; orak.push(i++)) {}
+      orak;
+    }}
+  >
+    {#each ht as v}
+      <option value={v}>
+        {v}
+      </option>
+    {/each}
+  </select>
+  {#if orak.length}
+    <table>
+      <tr>
+        <th>#</th>
+        {#each napnevek as napnév}
+          <th>{napnév}</th>
+        {/each}
+      </tr>
+      {#each orak as ora}
+        <tr>
+          <th>{ora}</th>
+          {#each napok as nap}
+            <td>
+              <div class="targy">
+                {targy(ora, nap)}
+              </div>
+              <div class="tanar">
+                {tanar(ora, nap)}
+              </div>
+              <div class="terem">
+                {terem(ora, nap)}
+              </div>
+            </td>
+          {/each}
+        </tr>
+      {/each}
+    </table>
+  {/if}
 </main>
 
 <style>
+  table {
+    border-spacing: 10px;
+
+  }
+  td {
+    background-color: rgb(187, 235, 219);
+    color: black;
+    border: solid 2px green;
+    width: 120px;
+    height: 60px;
+    box-shadow: 1px 1px 3px inset black;
+  }
+  div.targy {
+    font-size: 9px;
+  }
+  div.tanar {
+    font-size: 8px;
+  }
+  div.terem {
+    font-size: 12px;
+  }
 </style>
